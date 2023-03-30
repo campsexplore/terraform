1707c5711cfed0049371fe55651c77270d628c0c
Jonas <jonasexplore@gmail.com>
2023-03-30 08:36:41 -0300

    feat: add data and output

--- local.tf
+++ local.tf
@@ -1,4 +1,18 @@
 resource "local_file" "test" {
-  content  = "Hello World!"
+  content  = var.content
   filename = "test.txt"
+}
+
+data "local_file" "test" {
+  filename = "test.txt"
+}
+
+output "data-source-result" {
+  value = data.local_file.test.content
+}
+
+variable "content" {}
+
+output "id-test" {
+  value = resource.local_file.test.id
 }
